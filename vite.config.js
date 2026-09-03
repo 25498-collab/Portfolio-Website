1ba1086f68845f745b974523e5481534375dbdd6
Luke <luke@example.com>
2026-09-03 19:58:58 +1200

    Preserve aliased module paths

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   base: './',
   resolve: {
     alias: [
-      { find: /^@\/api(\/.*)?$/, replacement: fileURLToPath(new URL('./src/API', import.meta.url)) },
-      { find: /^@\/lib(\/.*)?$/, replacement: fileURLToPath(new URL('./src/LIB', import.meta.url)) },
-      { find: /^@\/components(\/.*)?$/, replacement: fileURLToPath(new URL('./src/components', import.meta.url)) },
-      { find: /^@\/pages(\/.*)?$/, replacement: fileURLToPath(new URL('./src/pages', import.meta.url)) },
+      { find: /^@\/api(\/.*)?$/, replacement: `${fileURLToPath(new URL('./src/API', import.meta.url))}$1` },
+      { find: /^@\/lib(\/.*)?$/, replacement: `${fileURLToPath(new URL('./src/LIB', import.meta.url))}$1` },
+      { find: /^@\/components(\/.*)?$/, replacement: `${fileURLToPath(new URL('./src/components', import.meta.url))}$1` },
+      { find: /^@\/pages(\/.*)?$/, replacement: `${fileURLToPath(new URL('./src/pages', import.meta.url))}$1` },
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
     ]
   },
