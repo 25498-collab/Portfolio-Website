ac6b23852948540d32d2e3a403b7240af333f8d6
Luke <luke@example.com>
2026-09-03 20:12:39 +1200

    Use lowercase source aliases

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   base: './',
   resolve: {
     alias: [
-      { find: /^@\/api(\/.*)?$/, replacement: `${fileURLToPath(new URL('./src/API', import.meta.url))}$1` },
-      { find: /^@\/lib(\/.*)?$/, replacement: `${fileURLToPath(new URL('./src/LIB', import.meta.url))}$1` },
+      { find: /^@\/api(\/.*)?$/, replacement: `${fileURLToPath(new URL('./src/api', import.meta.url))}$1` },
+      { find: /^@\/lib(\/.*)?$/, replacement: `${fileURLToPath(new URL('./src/lib', import.meta.url))}$1` },
       { find: /^@\/components(\/.*)?$/, replacement: `${fileURLToPath(new URL('./src/components', import.meta.url))}$1` },
       { find: /^@\/pages(\/.*)?$/, replacement: `${fileURLToPath(new URL('./src/pages', import.meta.url))}$1` },
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
