6c5ad509368da1288f7ada64d07807d89fc0ff0a
Luke <luke@example.com>
2026-09-03 19:58:40 +1200

    Fix GitHub Pages module aliases

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   base: './',
   resolve: {
-    alias: {
-      '@/api': fileURLToPath(new URL('./src/API', import.meta.url)),
-      '@/lib': fileURLToPath(new URL('./src/LIB', import.meta.url)),
-      '@/components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@/pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    alias: [
+      { find: /^@\/api(\/.*)?$/, replacement: fileURLToPath(new URL('./src/API', import.meta.url)) },
+      { find: /^@\/lib(\/.*)?$/, replacement: fileURLToPath(new URL('./src/LIB', import.meta.url)) },
+      { find: /^@\/components(\/.*)?$/, replacement: fileURLToPath(new URL('./src/components', import.meta.url)) },
+      { find: /^@\/pages(\/.*)?$/, replacement: fileURLToPath(new URL('./src/pages', import.meta.url)) },
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
+    ]
   },
   plugins: [
     react(),
