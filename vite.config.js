c0669258d84b4a58401f304d6ddb7b5c877a9926
Luke <luke@example.com>
2026-09-03 19:55:54 +1200

    Fix Linux build paths

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   base: './',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@/api': fileURLToPath(new URL('./src/API', import.meta.url)),
       '@/lib': fileURLToPath(new URL('./src/LIB', import.meta.url)),
       '@/components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@/pages': fileURLToPath(new URL('./src/pages', import.meta.url))
+      '@/pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   plugins: [
